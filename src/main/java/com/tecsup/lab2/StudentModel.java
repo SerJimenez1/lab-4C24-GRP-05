706da66b3ccd6d39432bb4814798b294013c399b
Sergio <serjimenez940@gmail.com>
2025-08-25 20:58:28 -0500

    Modificación en StudentModel.java - Sergio

--- src/main/java/com/tecsup/lab2/StudentModel.java
+++ src/main/java/com/tecsup/lab2/StudentModel.java
@@ -1,10 +1,14 @@
 public class StudentModel {
+
     private String name;
     private int age;
+    private double grade;
 
-    public StudentModel(String name, int age) {
+    // Constructor
+    public StudentModel(String name, int age, double grade) {
         this.name = name;
         this.age = age;
+        this.grade = grade;
     }
 
     // Getters y Setters
@@ -22,5 +26,23 @@ public class StudentModel {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    // Método nuevo para ver si aprueba
+    public boolean isApproved() {
+        return this.grade >= 13.0; // nota mínima aprobatoria
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante: " + name + ", Edad: " + age + ", Nota: " + grade;
     }
 }
